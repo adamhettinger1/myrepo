1d4b583d863a1a3380b088c919aa36c988f34ed6
Adam Hettinger <adam.kyle.hettinger@gmail.com>
2019-07-16 12:28:13 -0600

    adding written and coded notation for twitter model

--- new.R
+++ new.R
@@ -2,3 +2,22 @@
 x = 5
 
 plot(x)
+View(twitter_subset)
+
+#Ki ∼ Normal(µi, σ)
+#µi = α + βNNi + βMMi
+#α ∼ Normal(0, 0.2)
+#βn ∼ Normal(0, 0.5)
+#βm ∼ Normal(0, 0.5)
+#σ ∼ Exponential(1)
+
+m5.7 <- quap(
+  alist(
+    K ~ dnorm( mu , sigma ) ,
+    mu <- a + bN*N + bM*M ,
+    a ~ dnorm( 0 , 0.2 ) ,
+    bN ~ dnorm( 0 , 0.5 ) ,
+    bM ~ dnorm( 0 , 0.5 ) ,
+    sigma ~ dexp( 1 )
+  ) , data=dcc )
+precis(m5.7)
